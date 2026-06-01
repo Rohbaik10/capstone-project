54fa3b77499413842dc717b9420f78c139f9dceb
Rohbaik10 <thurkitty678@gmail.com>
2026-06-02 05:19:44 +0700

    prepare vercel deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "http://localhost:5000/api",
+    "https://capstone-project-production-9c31.up.railway.app/api",
 });
 
 export default api;
